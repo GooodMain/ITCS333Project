86ed721cea99a965297e785d9437c503d2b2d3da
Asma-Alshams <asma.alshams03@gmail.com>
2024-12-05 21:42:29 +0300

    database added

--- booking_system.sql
+++ booking_system.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 05, 2024 at 03:02 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2024 at 07:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,24 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `class`
+-- Table structure for table `bookings`
 --
 
-CREATE TABLE `class` (
-  `class_id` int(4) NOT NULL,
-  `c_type` varchar(10) NOT NULL
+CREATE TABLE `bookings` (
+  `booking_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `time_slot_id` int(11) NOT NULL,
+  `booking_date` date NOT NULL,
+  `booking_status` enum('Confirmed','Pending','Cancelled') DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `class_id` int(11) NOT NULL,
+  `class_num` varchar(10) NOT NULL,
+  `class_type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `class`
+-- Dumping data for table `classes`
 --
 
-INSERT INTO `class` (`class_id`, `c_type`) VALUES
-(1, 'class'),
-(2, 'class'),
-(3, 'lab'),
-(4, 'lab'),
-(5, 'open lab');
+INSERT INTO `classes` (`class_id`, `class_num`, `class_type_id`) VALUES
+(14, 'Class 2000', 2),
+(15, 'Class 2001', 2),
+(16, 'Class 2002', 2),
+(17, 'Class 2003', 2),
+(18, 'Class 2004', 2),
+(19, 'Class 2005', 2),
+(20, 'Lab 10', 3),
+(21, 'Lab 20', 3),
+(22, 'Lab 30', 3),
+(23, 'Lab 40', 3),
+(24, 'Open Lab 1', 4),
+(25, 'Open Lab 2', 4),
+(26, 'Benefit La', 1);
 
 -- --------------------------------------------------------
 
@@ -50,37 +74,53 @@ INSERT INTO `class` (`class_id`, `c_type`) VALUES
 --
 
 CREATE TABLE `class_type` (
-  `type` varchar(100) NOT NULL,
+  `class_type_id` int(11) NOT NULL,
+  `type_name` varchar(20) NOT NULL,
   `capacity` varchar(255) NOT NULL,
-  `equipments` varchar(255) NOT NULL
+  `equipments` varchar(255) NOT NULL,
+  `image` longblob DEFAULT NULL,
+  `image_name` varchar(255) DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `class_type`
 --
 
-INSERT INTO `class_type` (`type`, `capacity`, `equipments`) VALUES
-('benefit lab', '30-40 students', '30-40 PC\'s and one projector '),
-('class', '50-60 students', 'one PC, one projector and one white board.'),
-('lab', '30-40 students', '30-40 PC\'s, one projector and one white board.'),
-('open lab', '130-150 students', '60-70 PC\'s, one projector and two white boards, one microphone and speakers');
+INSERT INTO `class_type` (`class_type_id`, `type_name`, `capacity`, `equipments`, `image`, `image_name`) VALUES
+(1, 'benefit lab', '30-40 students', '30-40 PC\'s and one projector ', NULL, '');
+INSERT INTO `class_type` (`class_type_id`, `type_name`, `capacity`, `equipments`, `image`, `image_name`) VALUES
+(2, 'class', '50-60 students', 'one PC, one projector and one white board.', NULL, '');
+INSERT INTO `class_type` (`class_type_id`, `type_name`, `capacity`, `equipments`, `image`, `image_name`) VALUES
+(3, 'lab', '30-40 students', '30-40 PC\'s, one projector and one white board.', NULL, ''),
+(4, 'open lab', '130-150 students', '60-70 PC\'s, one projector and two white boards, one microphone and speakers', NULL, '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `room_bookings`
+-- Table structure for table `time_slots`
 --
 
-CREATE TABLE `room_bookings` (
-  `id` int(11) NOT NULL,
-  `room_id` int(11) NOT NULL,
-  `start_time` datetime NOT NULL,
-  `end_time` datetime NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `cl_type` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+CREATE TABLE `time_slots` (
+  `time_slot_id` int(11) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `time_slots`
+--
+
+INSERT INTO `time_slots` (`time_slot_id`, `start_time`, `end_time`) VALUES
+(1, '08:00:00', '09:00:00'),
+(2, '09:00:00', '10:00:00'),
+(3, '10:00:00', '11:00:00'),
+(4, '11:00:00', '12:00:00'),
+(5, '12:00:00', '13:00:00'),
+(6, '13:00:00', '14:00:00'),
+(7, '14:00:00', '15:00:00'),
+(8, '15:00:00', '16:00:00'),
+(9, '16:00:00', '17:00:00'),
+(10, '17:00:00', '18:00:00');
 
 -- --------------------------------------------------------
 
@@ -89,43 +129,58 @@ CREATE TABLE `room_bookings` (
 --
 
 CREATE TABLE `user` (
-  `user_id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(5) NOT NULL,
   `email` varchar(100) NOT NULL,
   `fullName` varchar(100) NOT NULL,
   `phoneNum` varchar(15) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `user_type` varchar(10) NOT NULL,
-  `picture` varchar(255) DEFAULT NULL
+  `user_type` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `email`, `fullName`, `phoneNum`, `password`, `user_type`, `picture`) VALUES
-(1, '202011514@stu.uob.edu.bh', 'Maryam Husain', '39565657', '$2y$10$99R48JdubD.51zBRuDri6uTMnjEqhcB9718/rIpq5Hy8hMwKd251O', '', NULL),
-(2, '202011585@stu.uob.edu.bh', 'Mohammed Alhusaini', '+973 33414205', '$2y$10$u/rHU4xekWINDFKUitkwdueipn4Od.XZH4FKMHWhnPZ5cGBsLwpWq', '', NULL),
-(3, '202100111@stu.uob.edu.bh', 'pass:123-Admin', '33333333', '123-Admin', 'admin', NULL),
-(4, '202100863@stu.uob.edu.bh', 'ASMA', '32217880', '$2y$10$1eGP1se39abCMzixLxMevu8kRqWXXWymElNAHwnVmfFnS1Xr45Roy', '', NULL),
-(5, '202103399@stu.uob.edu.bh', 'Hussain Salah Mohammed', '39561188', '$2y$10$WXQBVKxRyD2XIhCgMKJIge2Mqbprf.1Ysegx.HCaOb0rMzlrAZg0u', '', NULL),
-(6, 'salehmohmd@uob.edu.bh', 'Saleh Mohammed', '0097333695821', '$2y$10$z93bf6mdgNDN9lxGDXA9eORqqIR46OCPuT/TxZ6L6jrg0C37d0PT2', '', NULL);
+INSERT INTO `user` (`user_id`, `email`, `fullName`, `phoneNum`, `password`, `user_type`) VALUES
+(1, '202011514@stu.uob.edu.bh', 'Maryam Husain', '39565657', '$2y$10$99R48JdubD.51zBRuDri6uTMnjEqhcB9718/rIpq5Hy8hMwKd251O', ''),
+(2, '202011585@stu.uob.edu.bh', 'Mohammed Alhusaini', '+973 33414205', '$2y$10$u/rHU4xekWINDFKUitkwdueipn4Od.XZH4FKMHWhnPZ5cGBsLwpWq', ''),
+(3, '202100111@stu.uob.edu.bh', 'pass:123-Admin', '33333333', '123-Admin', 'admin'),
+(4, '202100863@stu.uob.edu.bh', 'ASMA', '32217880', '$2y$10$1eGP1se39abCMzixLxMevu8kRqWXXWymElNAHwnVmfFnS1Xr45Roy', ''),
+(5, 'salehmohmd@uob.edu.bh', 'Saleh Mohammed', '0097333695821', '$2y$10$z93bf6mdgNDN9lxGDXA9eORqqIR46OCPuT/TxZ6L6jrg0C37d0PT2', ''),
+(6, '202102021@stu.uob.edu.bh', 'haya', '33221155', '$2y$10$Y36125.7YRMGTcEehM4U/O8zQWi.9wqvwr3.U//9UeLkMsvwS/4ma', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `time_slot_id` (`time_slot_id`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`class_id`),
+  ADD UNIQUE KEY `class_num` (`class_num`),
+  ADD KEY `class_type_id` (`class_type_id`);
+
+--
 -- Indexes for table `class_type`
 --
 ALTER TABLE `class_type`
-  ADD UNIQUE KEY `idx_type` (`type`);
+  ADD PRIMARY KEY (`class_type_id`);
 
 --
--- Indexes for table `room_bookings`
+-- Indexes for table `time_slots`
 --
-ALTER TABLE `room_bookings`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_room_class_type` (`cl_type`);
+ALTER TABLE `time_slots`
+  ADD PRIMARY KEY (`time_slot_id`);
 
 --
 -- Indexes for table `user`
@@ -138,26 +193,52 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `room_bookings`
+-- AUTO_INCREMENT for table `bookings`
 --
-ALTER TABLE `room_bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `bookings`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `class_type`
+--
+ALTER TABLE `class_type`
+  MODIFY `class_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `time_slots`
+--
+ALTER TABLE `time_slots`
+  MODIFY `time_slot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `room_bookings`
+-- Constraints for table `bookings`
 --
-ALTER TABLE `room_bookings`
-  ADD CONSTRAINT `fk_room_class_type` FOREIGN KEY (`cl_type`) REFERENCES `class_type` (`type`) ON DELETE CASCADE;
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`),
+  ADD CONSTRAINT `bookings_ibfk_3` FOREIGN KEY (`time_slot_id`) REFERENCES `time_slots` (`time_slot_id`);
+
+--
+-- Constraints for table `classes`
+--
+ALTER TABLE `classes`
+  ADD CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`class_type_id`) REFERENCES `class_type` (`class_type_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
