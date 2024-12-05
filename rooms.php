8f9b594692ca09ea396c1477d9d19d1b3799b887
Asma-Alshams <asma.alshams03@gmail.com>
2024-12-05 23:48:06 +0300

    fix the room issue

--- rooms.php
+++ rooms.php
@@ -8,28 +8,14 @@ if (!isset($_SESSION['user'])) {
     exit();
 }
 
-// Corrected connection details
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'booking_system';
+include 'connection.php';
 
-// Establish the connection
-$con = mysqli_connect($host, $username, $password, $dbname);
-// Check connection
-if (!$con) {
-    die('Connection error: ' . mysqli_connect_error());
+try {
+    $result = $pdo->query('SELECT * FROM class_type');
+    $class = $result->fetchAll(PDO::FETCH_ASSOC); 
+} catch (PDOException $e) {
+    die("Error fetching class types: " . $e->getMessage());
 }
-
-// Query to fetch data from the 'class' table
-$sql = 'SELECT * FROM class_type';
-$result = mysqli_query($con, $sql);
-// Fetch and print all records
-$class = mysqli_fetch_all($result, MYSQLI_ASSOC);
-// Free result and close connection
-mysqli_free_result($result);
-mysqli_close($con);
-
 ?>
 
 
@@ -54,7 +40,7 @@ mysqli_close($con);
             <div class="col s6 md3">
                 <div class="card z-depth-0">
                     <div class="card-content center">
-                        <h6><?php echo htmlspecialchars($room['type']); ?></h6>
+                        <h6><?php echo htmlspecialchars($room['type_name']); ?></h6>
                     </div>
 
                     <div class="card-action right-align">
@@ -65,7 +51,7 @@ mysqli_close($con);
                         <div class="modal fade" id="modal-<?php echo $index; ?>" role="dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h4 class="modal-title"><?php echo htmlspecialchars($room['type']); ?></h4>
+                                    <h4 class="modal-title"><?php echo htmlspecialchars($room['type_name']); ?></h4>
                                 </div>
                                 <div class="modal-body">
                                     <p>Details about the room:</p>
