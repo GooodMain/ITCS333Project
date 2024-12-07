2b70aa0663b38599bd5d1b06ef6325434be84a10
Asma-Alshams <asma.alshams03@gmail.com>
2024-12-07 20:52:05 +0300

    img in the rooms page

--- rooms.php
+++ rooms.php
@@ -12,7 +12,7 @@ include 'connection.php';
 
 try {
     $result = $pdo->query('SELECT * FROM class_type');
-    $class = $result->fetchAll(PDO::FETCH_ASSOC); 
+    $class = $result->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Error fetching class types: " . $e->getMessage());
 }
@@ -34,42 +34,47 @@ try {
 
     <h4 class="center grey-text">ROOMS</h4>
 
-<div class="container">
-    <div class="row">
-        <?php foreach ($class as $index => $room) { // Add an index for unique IDs ?>
-            <div class="col s6 md3">
-                <div class="card z-depth-0">
-                    <div class="card-content center">
-                        <h6><?php echo htmlspecialchars($room['type_name']); ?></h6>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <?php foreach ($class as $index => $room) { // Add an index for unique IDs ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($room['type_name']); ?></h6>
+                            <div style="display: flex; justify-content: center; align-items: center; flex-wrap: wrap;"><img
+                                    src="image/<?php echo htmlspecialchars($room['image']); ?>"
+                                    style="max-width: 100%; height: auto; margin: auto;" /></div>
+                        </div>
 
-                    <div class="card-action right-align">
-                        <!-- Button to trigger the modal -->
-                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modal-<?php echo $index; ?>">More Details</button>
+                        <div class="card-action right-align">
+                            <!-- Button to trigger the modal -->
+                            <button type="button" class="btn btn-info btn-lg" data-toggle="modal"
+                                data-target="#modal-<?php echo $index; ?>">More Details</button>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="modal-<?php echo $index; ?>" role="dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title"><?php echo htmlspecialchars($room['type_name']); ?></h4>
-                                </div>
-                                <div class="modal-body">
-                                    <p>Details about the room:</p>
-                                    <p>Capacity: <?php echo htmlspecialchars($room['capacity']); ?></p>
-                                    <p>Equipments: <?php echo htmlspecialchars($room['equipments']); ?></p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="modal-<?php echo $index; ?>" role="dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <div id="txt" style="float:left;"><img
+                                                src="image/<?php echo htmlspecialchars($room['image']); ?>"
+                                                style="width:300px; height:200px;" /></div>
+                                        <h4 class="modal-title"><?php echo htmlspecialchars($room['type_name']); ?></h4>
+                                        <p>Details about the room:</p>
+                                        <p>Capacity: <?php echo htmlspecialchars($room['capacity']); ?></p>
+                                        <p>Equipments: <?php echo htmlspecialchars($room['equipments']); ?></p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        <?php } ?>
+            <?php } ?>
+        </div>
     </div>
-</div>
-    
+
     <?php include("footer.php"); ?>
 </body>
 
