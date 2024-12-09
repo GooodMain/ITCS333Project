73d4273820dec47ec487854a37a4e379ffc21a84
GoodMain0 <202009537@stu.uob.edu.bh>
2024-12-09 22:23:21 +0300

    Synced Header sizes in all pages

--- user_management.php
+++ user_management.php
@@ -110,12 +110,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     
+    <style> 
+        a {
+            font-size: 15px !important; 
+        }
+
+        h4 {
+            font-size: 37.5px !important;
+        }
+    </style>
 </head>
+
 
 <body>
     <?php include("header.php"); ?>
